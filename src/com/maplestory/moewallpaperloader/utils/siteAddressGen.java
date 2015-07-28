7564aea911731a96cc930984951294fab466df23
Maplestorys <maplestory.zeng@gmail.com>
2015-07-28 20:46:20 +0800

    remove 18x filter

--- src/com/maplestory/moewallpaperloader/utils/siteAddressGen.java
+++ src/com/maplestory/moewallpaperloader/utils/siteAddressGen.java
@@ -10,8 +10,8 @@ public class siteAddressGen {
 //	private static final String siteBaseAddress = "https://yande.re/post?";
 	private static final String siteBaseAddress = "http://konachan.com/post?";
 	private static final String sitePageAddress = "page=";
-	private static final String siteTagAddress = "&tags=rating:s";
-	
+//	private static final String siteTagAddress = "&tags=rating:s";
+	private static final String siteTagAddress = "&tags=";
 	
 	public static String getSiteAddress(int pageNum) {
 		return siteBaseAddress + sitePageAddress+pageNum+siteTagAddress;
@@ -19,7 +19,11 @@ public class siteAddressGen {
 
 	public static String getSiteAddress(int pageNum, String tags) {
 		String result = siteBaseAddress + sitePageAddress + pageNum + siteTagAddress ;
-		result +="+" +  tags;
+		if (result.charAt(result.length()-1)=='='){
+			result += tags;
+		}else{
+			result +="+" +  tags;
+		}
 		return result;
 	}
 }
